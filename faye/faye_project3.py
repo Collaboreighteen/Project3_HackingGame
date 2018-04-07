9d2e329fc6ebff4628986ffaa90922ca7233f537
fayemc <faye.m.camp@gmail.com>
2018-04-07 14:12:21 -0700

    Fixed bug that made .find to break on guesses with multiple instances of the same character

--- faye/faye_project3.py
+++ faye/faye_project3.py
@@ -8,16 +8,18 @@
 # Be able to check guess against list of options
 # Be able to compare guess and answer using character and position
 # Count numer of correct characters in position
-# Limit to 3 attempts
+# Limit to 4 attempts
 
 import random
 
 def evalGuess(guess):
 	correct = 0
+	loop = 0
 
 	for l in guess:
-		if guess.find(l) == answer.find(l):
+		if guess[loop] == answer[loop]:
 			correct = correct + 1
+		loop = loop + 1
 
 	print("You have "+str(correct)+"/7 correct")
 
@@ -45,18 +47,18 @@ end = False
 
 print(wordList)
 
-while (guessNum < 3 and end != True):
-	guessLeft = 3 - guessNum
+while (guessNum < 4 and end != True):
+	guessLeft = 4 - guessNum
 
-	if guessLeft == 3:
-		guess = input("You have 3 tries to guess the word from this list. Enter your first guess\n")
+	if guessLeft == 4:
+		guess = input("You have 4 tries to guess the word from this list. Enter your first guess\n")
 	else:
 		guess = input("Try again. You have " + str(guessLeft) + " guesses left\n")
 
 	guess = [word for word in wordList if guess == word]
 
 	if guess == []:
-		print("That is not one of the words. You have " + str(guessLeft - 1) + " guesses left\n")
+		print("That is not one of the words.\n")
 
 	if guess != []:
 		if guess[0] == answer:
